62c929c32c33a417394cb5e59c75053de34ce8a7
mairazampa <87398878+mairazampa@users.noreply.github.com>
2022-11-17 19:05:14 -0300

    Update models.py

--- App/models.py
+++ App/models.py
@@ -53,5 +53,24 @@ class Periodo_Electivo(models.Model):
     def __str__(self):
         return self.nombre
     
-
+class Instituto(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+    id_instituto = models.ForeignKey(Instutito, on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.nombre
+    
+    
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    id_carrera = models.ManyToManyField(Carrera)
+   
+    def __str__(self):
+        return self.nombre
 
