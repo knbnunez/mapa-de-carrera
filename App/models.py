08e1db73f618dfe164f943329e6ced751ad58db4
mairazampa <87398878+mairazampa@users.noreply.github.com>
2022-11-17 19:00:57 -0300

    Update models.py

--- App/models.py
+++ App/models.py
@@ -16,3 +16,42 @@ class Resolucion(models.Model):
     
     def __str__(self):
         return self.nombre
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+
+class Modalidad(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+class Dedicacion(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+ class Localidad(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+ 
+class Periodo_Electivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    desde = models.DateField()
+    hasta = models.DateField()
+    
+    def __str__(self):
+        return self.nombre
+    
+
+
