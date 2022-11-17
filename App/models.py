a4cbbe4a2b1f60c14a905ebb5f33ed858ef6e936
mairazampa <87398878+mairazampa@users.noreply.github.com>
2022-11-17 18:57:45 -0300

    Update models.py
    Se crea clase Resolucion

--- App/models.py
+++ App/models.py
@@ -7,4 +7,12 @@ class Cargas_Extras(models.Model):
     def __str__(self):
         return self.nombre
     
-  
+ 
+class Resolucion(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    documento = models.ImageField(upload_to='pdf')
+    
+    def __str__(self):
+        return self.nombre
