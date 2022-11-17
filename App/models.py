b948fa53cd2dc5a0273dcde51c882eb5eab9e1c5
knbnunez <knbnunez@gmail.com>
2022-11-17 20:07:45 -0300

    2do commit, nada relevante de mi lado

--- App/models.py
+++ App/models.py
@@ -3,3 +3,18 @@ from django.db import models
 # Create your models here.
 
 # Acá haces el modelo vos
+
+class MODELNAME(models.Model):
+    """Model definition for MODELNAME."""
+
+    
+
+    class Meta:
+        """Meta definition for MODELNAME."""
+
+        verbose_name = 'MODELNAME'
+        verbose_name_plural = 'MODELNAMEs'
+
+    def __str__(self):
+        """Unicode representation of MODELNAME."""
+        pass
