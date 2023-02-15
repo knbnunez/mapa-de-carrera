e3c151231e6479f2f7202df3651206f82176c416
knbnunez <knbnunez@gmail.com>
2023-02-15 18:50:02 -0300

    Pre-development Requerimiento: Asignar modalidad

--- App/views/views.py
+++ App/views/views.py
@@ -13,17 +13,16 @@ class IndexView(TemplateView):
 
 class DocenteView(TemplateView):
     template_name = 'docente.html'
-    username='mapumapa'
+    username='mapumapa' # Para producción hay que crifrar las credenciales
     password='Mowozelu28'
     url_mapuche = 'http://10.7.180.231/mapuche/rest/'
 
-    def get(self, request, legajo):
-        url = reverse('docente', kwargs={'legajo': legajo})
-        recover_legajo = url.split('/')[-2] # split('/') = ['', 'game', 'id', '']; split('/')[-2] = ['id']
-        url_docente = self.url_mapuche+'agentes/'+recover_legajo # /agentes/{legajo}
-        url_mail = self.url_mapuche+'agentes/'+recover_legajo+'/mail' # /agentes/{legajo}/mail
-        # responses[0] = requests.get(url_docente, auth=(self.username, self.password))
-        # responses[1] = requests.get(url_mail, auth=(self.username, self.password))
+    def get(self, request, legajo): # Se puede recuperar el param atr llamándolo como esta definido en urls.py en este caso: legajo
+        # Y no hace falta usar el reverse ni hacer el split
+        legajo = str(legajo)
+        print(legajo)
+        url_docente = self.url_mapuche+'agentes/'+legajo # /agentes/{legajo}
+        url_mail = self.url_mapuche+'agentes/'+legajo+'/mail' # /agentes/{legajo}/mail
         responses = [requests.get(url, auth=(self.username, self.password)) for url in [url_docente, url_mail]] # lo mismo, pero hecho con lista por comprensión
         exito = -1
         for response in responses:
@@ -31,13 +30,10 @@ class DocenteView(TemplateView):
                 exito += 1
         if exito == 1: # exito en ambos casos
             jsons = [response.json() for response in responses]
-            # for json in jsons:
-            #     print(json)
             if not jsons[1]: # Consulto si la lista en la posición [1] está vacía
                 jsons[1] = [{'correo_electronico': None}]
             if jsons[0]['fecha_jubilacion'] is 'null':
                 jsons[0]['fecha_jubilacion'] = None
-            # else: no está vacío y no hay que hacer nada, ya tiene un valor
             docente = Docente(
                 numero_documento    = jsons[0]['numero_documento'],
                 legajo              = jsons[0]['legajo'],
@@ -49,8 +45,7 @@ class DocenteView(TemplateView):
             # TO DO: guardarlo en la base de datos. docente.save() y algo más?
 
         # TO DO: else
-        return render(request, self.template_name, { 'docente': docente})
-
+        return render(request, self.template_name, {'docente': docente})
 
 
 class DocenteBusquedaView(TemplateView):
@@ -61,18 +56,28 @@ class DocenteBusquedaView(TemplateView):
 
     def get(self, request):
         url = reverse('buscadocente')
-        #recover_legajo = url.split('/')[-2] # split('/') = ['', 'game', 'id', '']; split('/')[-2] = ['id']
         url_buscadocente = self.url_mapuche+'agentes' # /agentes/{legajo}
-        #url_mail = self.url_mapuche+'agentes/'+recover_legajo+'/mail' # /agentes/{legajo}/mail
-        # responses[0] = requests.get(url_docente, auth=(self.username, self.password))
-        # responses[1] = requests.get(url_mail, auth=(self.username, self.password))
         response = requests.get(url_buscadocente, auth=(self.username, self.password))
-        #if response.status_code == 200:
-                      
-        docentes= response.json()
+        # if response.status_code == 200:
+        docentes = response.json()
+        # print(docentes)
+        return render(request, self.template_name, {'docentes': docentes})
+        # else :
+            # return render(request, self.template_name, {"docentes":})
 
-        print (docentes)
-        return render(request, self.template_name, {"docentes":docentes})
-        
-       # else :
-        #    return render(request, self.template_name, {"docentes":})
+
+class DocenteModalidadView(TemplateView):
+    template_name = 'docente-modalidad.html'
+    username = 'mapumapa'
+    password = 'Mowozelu28'
+    url_mapuche = 'http://10.7.180.231/mapuche/rest/'
+
+    def get(self, request, legajo):
+        legajo = str(legajo)
+        url = self.url_mapuche+'agentes/'+legajo+'/cargos' # /agentes/{legajo}/cargos
+        response = requests.get(url, auth=(self.username, self.password))
+        cargos = response.json()
+        cargos = [_['cargo'] for _ in response.json()]
+        print(cargos)
+        # TO DO: mostrar más datos para un cargo seleccionado, almacenar la modalidad para testear el tema de la restricción de horas
+        return render(request, self.template_name, {'cargos': cargos})
