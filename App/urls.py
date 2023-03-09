170289658d2569f898dd6671240772ff46520882
knbnunez <knbnunez@gmail.com>
2023-03-09 19:30:47 -0300

    Se corrigieron las urls

--- App/urls.py
+++ App/urls.py
@@ -5,7 +5,7 @@ from .views.docente_modalidad import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('docentes/', DocenteBusquedaView.as_view(), name='buscadocente'),
+    path('docentes/', DocenteBusquedaView.as_view(), name='docente'),
     path('docentes/<int:legajo>/detalles', DocenteDetalleView.as_view(), name='docente-detalle'),
-    path('docentes/<int:legajo>/asignar-modalidades', DocenteModalidadView.as_view(), name='docente-modalidad'),
+    path('docentes/<int:legajo>/asignar-modalidades', DocenteModalidadView.as_view(), name='docente-asignar-modalidad'),
 ]
