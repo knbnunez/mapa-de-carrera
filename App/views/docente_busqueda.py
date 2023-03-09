b48ebb0f4ea8eb581a890f0b8a3ea6fcbffef208
mairazampa <87398878+mairazampa@users.noreply.github.com>
2023-03-09 17:53:02 -0300

    Update docente_busqueda.py

--- App/views/docente_busqueda.py
+++ App/views/docente_busqueda.py
@@ -1,29 +1,75 @@
 # app/views.py
-from django.views.generic import TemplateView
-from django.shortcuts import render
-from django.urls import reverse
-from App.models.mapa_de_carreras import *
-import requests
-from django.db.models import Q
-import json
-from requests.exceptions import ConnectTimeout
-import datetime
+html lang="en">
+ 
+<head>
 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+   
+    
+    <title>MDC</title>
+</head>
 
+<body>
 
-class DocenteBusquedaView(TemplateView):
-    template_name = 'busca_docente.html'
-    username='mapumapa'
-    password='Mowozelu28'
-    url_mapuche = 'http://10.7.180.231/mapuche/rest/'
+<!--
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 search">
+                <form method="get">
+                    <div id="custom-search-input">
+                        <div class="input-group col-md-as">
+                            <input type="text" class="form-control" placeholder="Buscar" name="Buscar"></input>
+                            <span class="input-group-list">
+                                <i class="icon icon-search"></i>
+                                <button type="submit" class="boton btn-success">Buscar</button>
+                            </span>
+                        </div>
+                     </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-    def get(self, request):
-        url = reverse('buscadocente')
-        url_buscadocente = self.url_mapuche+'agentes' # /agentes/{legajo}
-        response = requests.get(url_buscadocente, auth=(self.username, self.password))
-        # if response.status_code == 200:
-        docentes = response.json()
-        # print(docentes)
-        return render(request, self.template_name, {'docentes': docentes})
-        # else :
-            # return render(request, self.template_name, {"docentes":})
+    <form action="http://127.0.0.1:8000/docente" method="get">
+        <label for="search_query">Buscar:</label>
+        <input type="text" name=" " id="search_query">
+        <button type="submit">Buscar</button>
+      </form>
+
+      <form id="mi-formulario" method="post">
+        <label for="search_query">Buscar:</label>
+        <input type="text" name="q" id="search_query">
+        <input type="hidden" name="legajo" >
+        
+        <button type="submit">Buscar</button>
+      </form>
+      
+      <script>
+        const formulario = document.querySelector('#mi-formulario');
+           formulario.addEventListener('submit', (event) => {
+          event.preventDefault();
+          const q = encodeURIComponent(document.querySelector('#search_query').value);
+          const legajo = encodeURIComponent(document.querySelector('input[name="legajo"]').value);
+          const url = `http://127.0.0.1:8000/docente/${legajo}`;
+          window.location.href = url;
+        });
+      </script>-->
+    <h1>Ingrese Legajo a buscar</h1>
+    <form action="{% url 'buscadocente' %}" method="busca">
+        <input type="text" name="q" value="{{ queryset }}">
+        <button type="submit">Buscar</button>
+    </form>
+    
+
+    {% for d in docentes %}
+        <h3> - Nro DNI: {{d.numero_documento}}</h3>
+        <h3> - Legajo: {{d.legajo}}</h3>
+        <h3> - Nombre y Apellido: {{d.nombre_apellido}}</h3>
+    {% endfor %}
+</body>
+
+</html>
