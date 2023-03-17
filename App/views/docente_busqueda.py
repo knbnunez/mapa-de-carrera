770e7e6849ac5893c4881bc35d7435c83c1cecf0
maira <mairazampa@gmail.com>
2023-03-17 18:57:46 -0300

    se agrega filtros con js

--- App/views/docente_busqueda.py
+++ App/views/docente_busqueda.py
@@ -27,28 +27,21 @@ class DocenteBusquedaView(TemplateView):
     url_mapuche = 'http://10.7.180.231/mapuche/rest/'
 
     def get(self, request):
-        url = reverse('buscadocente')
+        url = reverse('docente')
         url_buscadocente = self.url_mapuche+'agentes' # /agentes/{legajo}
         response = requests.get(url_buscadocente, auth=(self.username, self.password))
         # if response.status_code == 200:
         docentes = response.json()
-        # print(docentes)
+        print(docentes)
         
         return render(request, self.template_name, {'docentes': docentes})
         # else :
             # return render(request, self.template_name, {"docentes":})
     def busca(request):
-       # url=self.url_mapuche+'agentes/'+legajo
+        url=self.url_mapuche+'agentes/'+legajo
         queryset = request.Get.get('q')
         if queryset:
             docentes = Docente.objects.filter(legajo=queryset).first()
             if docentes:
                 url = reverse('docente-detalle', kwargs={'legajo':queryset})
-              #  return redirect('docentes', legajo=queryset)
-     #    if queryset:
-      #         docentes = Post.objects.filter(
-      #             Q(legajo = queryset) 
-      #         )
-      #   responses = [requests.get(url, auth=(self.username, self.password), timeout=5) for url in [url]]
-      #  return render(request, 'docente-detalle.html', {'docentes': json.dumps(docentes),'queryset':queryset} )
         return render(request, 'docente_detalle.html', {'queryset':queryset} )
