59966c5827d917ffbe82613364fa61cc7c81daa3
maira <mairazampa@gmail.com>
2023-09-07 21:14:23 -0300

    se agrega la carga de tareas extras

--- App/forms/cargoForm.py
+++ App/forms/cargoForm.py
@@ -1,33 +1,24 @@
-
-# forms.py
-
 from django import forms
 from App.models.mapa_de_carreras import Cargo, Tipo_Extra, Carga_Horaria, Tipo_Extra_CH, Cargo_CTE_CH
 
 class AsignarTareasForm(forms.Form):
     tipo_extra = forms.ModelChoiceField(queryset=None)
-    carga_horaria = forms.ModelChoiceField(queryset=None)
-    cargo = forms.ModelChoiceField(queryset=None)  # Agrega este campo
+    
+    cant_horas = forms.IntegerField()  # Campo para el número de horas
+    fecha_desde = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))  # Campo para la fecha desde
+    fecha_hasta = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))  # Campo para la fecha hasta
+  
+
+    cargo = forms.ModelChoiceField(queryset=None)  
     
     def __init__(self, *args, **kwargs):
         tipos_extra = kwargs.pop('tipos_extra')
-        carga_horaria = kwargs.pop('carga_horaria')
-        cargos_activos = kwargs.pop('cargos_activos')  # Agrega esto
+        
+        cargos_activos = kwargs.pop('cargos_activos')  
         
         super().__init__(*args, **kwargs)
         
         self.fields['tipo_extra'].queryset = tipos_extra
-        self.fields['carga_horaria'].queryset = carga_horaria
+        
         self.fields['cargo'].queryset = cargos_activos  # Agrega este queryset
 
-
-
-class AsignarCargaHorariaForm(forms.Form):
-    carga_horaria = forms.ModelChoiceField(queryset=None)
-    
-    def __init__(self, *args, **kwargs):
-        carga_horaria = kwargs.pop('carga_horaria')
-        
-        super().__init__(*args, **kwargs)
-        
-        self.fields['carga_horaria'].queryset = carga_horaria
--- App/views/docente_tareas.py
+++ App/views/docente_tareas.py
@@ -6,12 +6,10 @@ from requests.exceptions import ConnectTimeout
 import datetime
 from App.models.mapa_de_carreras import *
 from App.models.guarani import *
-from App.forms.cargoForm import  AsignarTareasForm, AsignarCargaHorariaForm
+from App.forms.cargoForm import  AsignarTareasForm
 
 class DocenteTareasView(TemplateView): # Detalle para un único docente
     template_name = 'docente_tareas.html'
-    #form_class = AsignarTareasForm
-    # model = Carga_Horaria
     username = 'mapumapa' # Para producción hay que crifrar las credenciales
     password = 'Mowozelu28'
     url_mapuche = 'http://10.7.180.231/mapuche/rest/'
@@ -22,11 +20,11 @@ class DocenteTareasView(TemplateView): # Detalle para un único docente
         docente = get_object_or_404(Docente, legajo=legajo)
         cargos_activos = Cargo.objects.filter(docente=docente, activo=1)
         tipos_extra = Tipo_Extra.objects.all()
-        carga_horaria = Carga_Horaria.objects.all()
+        
         
         form = AsignarTareasForm(
             tipos_extra=tipos_extra,
-            carga_horaria=carga_horaria,
+            
             cargos_activos=cargos_activos  # Agrega esto para pasar los cargos al formulario
         )
         
@@ -42,27 +40,30 @@ class DocenteTareasView(TemplateView): # Detalle para un único docente
         form = AsignarTareasForm(
             request.POST,
             tipos_extra=Tipo_Extra.objects.all(),
-            carga_horaria=Carga_Horaria.objects.all(),
             cargos_activos=Cargo.objects.filter(docente=docente, activo=1)
         )
         
         if form.is_valid():
             tipo_extra = form.cleaned_data['tipo_extra']
-            carga_horaria = form.cleaned_data['carga_horaria']
             cargo = form.cleaned_data['cargo']  # Obtener el cargo seleccionado del formulario
+            cant_horas = form.cleaned_data['cant_horas']
+            fecha_desde = form.cleaned_data['fecha_desde']
+            fecha_hasta = form.cleaned_data['fecha_hasta']
             
             # Guardar en Tipo_Extra_CH
             tipo_extra_ch = Tipo_Extra_CH.objects.create(
                 
                 tipo_extra=tipo_extra,
-                carga_horaria=carga_horaria
+                cant_horas=cant_horas,
+                fecha_desde=fecha_desde,
+                fecha_hasta=fecha_hasta,
             )
             
             # Guardar en Cargo_CTE_CH
             cargo_cte_ch = Cargo_CTE_CH.objects.create(
-            cargo=cargo,
-            tipo_extra_ch=tipo_extra_ch,
-        )
+             cargo=cargo,
+             tipo_extra_ch=tipo_extra_ch,
+         )
            
             
         
